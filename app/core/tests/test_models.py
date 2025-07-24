e5a0ee7ba3977a0f1ec3bb8a28eb0dab50975bfe
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2025-07-24 19:29:22 +0200

    Make email input as required

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,5 @@
 """
-Test for models. 
+Test for models.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
@@ -31,3 +31,8 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Tet that creating a user without an email raises a ValueEmail."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
