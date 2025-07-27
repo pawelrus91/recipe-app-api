49426e77bfb6afcf74f518ad7e05692fc9b2b31b
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2025-07-27 00:06:59 +0000

    Implemented token API

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework.authtoken',
     'core',
     'rest_framework',
     'drf_spectacular',
