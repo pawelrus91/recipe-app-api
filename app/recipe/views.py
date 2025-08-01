68e97f475ef8f243239861d4c6969787854b26aa
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2025-08-01 04:00:57 +0200

    Create recipe detail endopint

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,7 +1,7 @@
 """
 Views for the recipe APIs.
 """
-from rest_framework import generics, permissions, viewsets
+from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
@@ -11,7 +11,7 @@ from core.models import Recipe
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -20,22 +20,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Retrieve recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
 
+    def get_serializer_class(self):
+        """Return the serializer class for request"""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
 
-class CreateRecipeApiView(generics.ListCreateAPIView):
-    queryset = Recipe.objects.all()
-    serializer_class = serializers.RecipeSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def create(self, request, *args, **kwargs):
-        request.data['user'] = request.user.pk
-        return super().create(request, *args, **kwargs)
-
-    def get_queryset(self):
-        return super().get_queryset().filter(user=self.request.user)
-
-    # def create(self):
-
-
-class RetriveRecipeApiView(generics.RetrieveUpdateAPIView):
-    queryset = Recipe.objects.all()
-    serializer_class = serializers.RecipeSerializer
+        return self.serializer_class
