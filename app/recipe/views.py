4d041867af0b5e5f90d077d5540478a8c337eded
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2025-08-01 03:32:05 +0200

    Create recipe list

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+"""
+Views for the recipe APIs.
+"""
+from rest_framework import generics, permissions, viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from recipe import serializers
+from core.models import Recipe
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs."""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+
+class CreateRecipeApiView(generics.ListCreateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = serializers.RecipeSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        request.data['user'] = request.user.pk
+        return super().create(request, *args, **kwargs)
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
+
+    # def create(self):
+
+
+class RetriveRecipeApiView(generics.RetrieveUpdateAPIView):
+    queryset = Recipe.objects.all()
+    serializer_class = serializers.RecipeSerializer
