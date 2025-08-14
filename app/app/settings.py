1ed2ccae6173fb35b0186364f423e1429d33afbd
pawelrus91 <18408679+pawelrus91@users.noreply.github.com>
2025-08-14 22:41:52 +0200

    Add media root and url to project settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -123,7 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
+
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -136,4 +140,8 @@ AUTH_USER_MODEL = 'core.User'
 # https://drf-spectacular.readthedocs.io/en/latest/readme.html#installation
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'COMPONENT_SPLIT_REQUEST': True,
 }
